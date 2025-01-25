12f1d46c854b7366ba883b21751a5a9b22837312
Justin Su <jsu430130@gmail.com>
2025-01-25 21:43:54 +0000

    completing partner's tutorial

---
+++ hello_world/target/debug/deps/hello_world-f5c7b16336769822.d
@@ -0,0 +1,5 @@
+/workspaces/Rust-Project/hello_world/target/debug/deps/hello_world-f5c7b16336769822: src/main.rs
+
+/workspaces/Rust-Project/hello_world/target/debug/deps/hello_world-f5c7b16336769822.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/workspaces/Rust-Project/hello_world/target/debug/hello_world: /workspaces/Rust-Project/hello_world/src/main.rs
